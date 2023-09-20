6bc3ea57b706d90f16c974d1fd8234839e933176
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-20 15:41:21 +0530

    stem controller

--- examples/flask_tor_example.py
+++ examples/flask_tor_example.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_tor import run_with_tor
+# from flask_tor import run_with_tor
 
 app = Flask(__name__)
-port = run_with_tor()
+# port = run_with_tor()
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(port=port)
+    app.run(debug=True,
+            # port=port,
+            )
